8f0f06cce8ffdd9d5093f04373a3ef797ae48ab5
Rajvir Samrai <samrai.rajvir@gmail.com>
2021-02-03 02:12:06 -0500

    Add more documentation for the Image class

--- image.cpp
+++ image.cpp
@@ -4,12 +4,20 @@
 
 #include "image.h"
 
+
+/*
+Init the RGB data from the parameters into the corresponding array indices.
+*/
 Colour::Colour(unsigned char r, unsigned char g, unsigned char b){
     rgb[0] = b;
     rgb[1] = g;
     rgb[2] = r;
 }
 
+
+/*
+Init the image property variables and the pixel data array.
+*/
 Image::Image(int width, int height){
     this->width = width;
     this->height = height;
@@ -19,10 +27,16 @@ Image::Image(int width, int height){
     memset(pixels, 0, this->size);
 }
 
+/*
+Delete the pixel data array before destroying the image object.
+*/
 Image::~Image(){
     delete pixels;
 }
 
+/*
+Set the colour of a pixel in the pixel array. I.E update the RGB data.
+*/
 void Image::set(int x, int y, Colour &c){
     if(x < 0 || x >= width || y < 0 || y >= height){
         return;
@@ -31,6 +45,9 @@ void Image::set(int x, int y, Colour &c){
     }
 }
 
+/*
+Save pixel data to an image file in the TrueVision .tga file format.
+*/
 void Image::save(const char *filename){
     std::ofstream output(filename, std::ios::binary);
     if(!output.is_open()){
@@ -39,15 +56,16 @@ void Image::save(const char *filename){
 
     // HEADER
     char buf[18];
-    memset(buf, 0, 2);
-    memset(buf+2, 2, 1);
-    memset(buf+3, 0, 9);
+    memset(buf, 0, 2);                              // No Image ID Field, No colour map
+    memset(buf+2, 2, 1);                            // Data Type 2:Unmapped RGB
+    memset(buf+3, 0, 9);                            // Offsets 3 to 7 are ignored because we don't have a colour map.
+                                                    // 8-9 are X origin, 10-11 are Y origin
     memset(buf+12, (width & 0x00FF), 1);            // Lower order bits for width
     memset(buf+13, (width & 0xFF00) / 256, 1);      // Higher order bits for width
     memset(buf+14, (height & 0x00FF), 1);           // Lower order bits for height
     memset(buf+15, (height & 0xFF00) / 256, 1);     // Higher order bits for height
-    memset(buf+16, 24, 1);                          // 24 bit
-    memset(buf+17, 0, 1);
+    memset(buf+16, 24, 1);                          // 24 bits per pixel
+    memset(buf+17, 0, 1);                           // Image descriptor
     output.write(buf, 18);
 
     // PIXELS
--- image.h
+++ image.h
@@ -1,12 +1,20 @@
 #ifndef IMAGE_H
 #define IMAGE_H
 
+/*
+Represents a colour as a array of 3 bytes. This corresponds to RGB.
+*/
 class Colour {
     public:
         unsigned char rgb[3];
         Colour(unsigned char r, unsigned char g, unsigned char b);
 };
 
+/*
+Represents an image. Raw pixel data is stored in an unsigned char array. The
+amount of bytes is equal to width * heigh * 3. Width, height, and total pixels
+are also kept in integer variables.
+*/
 class Image {
     public:
         int width, height, size;
